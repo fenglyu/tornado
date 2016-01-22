114c6e7946602336be1cb3783ea2c2d209412096
Feng.LYU <lvfeng@cnsuning.com>
2016-01-22 16:13:09 +0800

    continue with template

--- app.py
+++ app.py
@@ -9,104 +9,111 @@ import logging
 import os
 
 logging.basicConfig(
-		level = logging.DEBUG,
-		format = '%(asctime)s %(levelname)s %(module)s.%(funcName)s Line:%(lineno)d %(message)s',
-		)
+    level=logging.DEBUG,
+    format='%(asctime)s %(levelname)s %(module)s.%(funcName)s Line:%(lineno)d %(message)s',
+)
 
 
 class MainHandler(tornado.web.RequestHandler):
-	def get(self):
-		self.write('<html><body>'
-			'<br><a href="/msg">Message</a>'
-			'<br><a href="/fetch_async">API @tornado.web.asynchronous</a>'
-			'<br><a href="/fetch_coro">API @tornado.gen.coroutine</a>'
-			'<br><a href="/temp">Template</a>'
-			'</body></html>')
+    def get(self):
+        self.write('<html><body>'
+                   '<br><a href="/msg">Message</a>'
+                   '<br><a href="/fetch_async">API @tornado.web.asynchronous</a>'
+                   '<br><a href="/fetch_coro">API @tornado.gen.coroutine</a>'
+                   '<br><a href="/temp">Template</a>'
+                   '</body></html>')
+
 
 class MyFormHandler(tornado.web.RequestHandler):
-	def get(self):
-		self.write('<html><body><form action="/msg" method="POST">'
-           '<input type="text" name="message">'
-           '<input type="submit" value="Submit">'
-           '</form></body></html>')
+    def get(self):
+        self.write('<html><body><form action="/msg" method="POST">'
+                   '<input type="text" name="message">'
+                   '<input type="submit" value="Submit">'
+                   '</form></body></html>')
 
-	def post(self):
-		self.set_header("Content-Type", "text/plain")
-		self.write("You wrote " + self.get_body_argument("message"))
+    def post(self):
+        self.set_header("Content-Type", "text/plain")
+        self.write("You wrote " + self.get_body_argument("message"))
+
 
 class FakeJsonAPI(tornado.web.RequestHandler):
-	@tornado.gen.coroutine
-	def get(self):
-		feng = dict(
-				name = "Feng LYU",
-				gender = "male",
-				age = 28,
-		)
+    @tornado.gen.coroutine
+    def get(self):
+        feng = dict(
+            name="Feng LYU",
+            gender="male",
+            age=28,
+        )
 
-		sky = dict(
-				name = "Sky Zhang",
-				gender = "female",
-				age = 26,
-		)
-		users = ( feng, sky )
-		json = tornado.escape.json_encode(users)
-		self.write(json)
+        sky = dict(
+            name="Sky Zhang",
+            gender="female",
+            age=26,
+        )
+        users = (feng, sky)
+        json = tornado.escape.json_encode(users)
+        self.write(json)
+
 
 class WebSpiderHandler_Async(tornado.web.RequestHandler):
-	@tornado.web.asynchronous
-	def get(self):
-		http = tornado.httpclient.AsyncHTTPClient()
-		http.fetch("http://192.168.56.111:8888/api", callback=self.on_response)
+    @tornado.web.asynchronous
+    def get(self):
+        http = tornado.httpclient.AsyncHTTPClient()
+        http.fetch("http://192.168.56.111:8888/api", callback=self.on_response)
 
-	def on_response(self, response):
-		if response.error: raise tornado.web.HTTPError(500)
-		json = tornado.escape.json_decode(response.body)
-		self.write("Fetched " + str(json) +" name"
-				"from our own api")
-		self.finish()
+    def on_response(self, response):
+        if response.error:
+            raise tornado.web.HTTPError(500)
+        json = tornado.escape.json_decode(response.body)
+        self.write("Fetched " + str(json) + " name"
+                   "from our own api")
+        self.finish()
+
 
 class WebSpiderHandler_coroutine(tornado.web.RequestHandler):
-	@tornado.gen.coroutine
-	def get(self):
-		http = tornado.httpclient.AsyncHTTPClient()
-		response = yield http.fetch("http://192.168.56.111:8888/api")
-		json = tornado.escape.json_decode(response.body)
-		self.write("Fetched " + str(json) + "from fake API")
+    @tornado.gen.coroutine
+    def get(self):
+        http = tornado.httpclient.AsyncHTTPClient()
+        response = yield http.fetch("http://192.168.56.111:8888/api")
+        json = tornado.escape.json_decode(response.body)
+        self.write("Fetched " + str(json) + "from fake API")
 
 
 class TemplateHandler(tornado.web.RequestHandler):
-	@tornado.gen.coroutine
-	def get(self):
-		http = tornado.httpclient.AsyncHTTPClient()
-		response = yield http.fetch("http://192.168.56.111:8888/api")
-		json = tornado.escape.json_decode(response.body)
-		#userlist = [ "Hello" , "hellO", "aaa" , "bbbb" ]
-		self.render("index.html", title = "Very first template demo", userlist = json )
+    @tornado.gen.coroutine
+    def get(self):
+        http = tornado.httpclient.AsyncHTTPClient()
+        response = yield http.fetch("http://192.168.56.111:8888/api")
+        json = tornado.escape.json_decode(response.body)
+        #userlist = [ "Hello" , "hellO", "aaa" , "bbbb" ]
+        self.render("index.html", title="Very first template demo", userlist=json)
+
 
 class Application(tornado.web.Application):
-	def __init__(self):
-		handlers = [
-			(r"/", MainHandler),
-			(r"/msg", MyFormHandler),
-			(r"/fetch_async", WebSpiderHandler_Async),
-			(r"/api", FakeJsonAPI),
-			(r"/fetch_coro", WebSpiderHandler_coroutine),
-			(r"/temp", TemplateHandler),
-		]
-		setttings = dict(
-			autoreload			= True,
-			debug				= True,
-			compress_response	= True,
-			template_path		= os.path.join(os.path.dirname(__file__), "templates"),
-			compiled_template_cache = False,
-		#	xsrf_cookies = True,
-		)
-		super(Application, self).__init__(handlers, **setttings)
+    def __init__(self):
+        handlers = [
+            (r"/", MainHandler),
+            (r"/msg", MyFormHandler),
+            (r"/fetch_async", WebSpiderHandler_Async),
+            (r"/api", FakeJsonAPI),
+            (r"/fetch_coro", WebSpiderHandler_coroutine),
+            (r"/temp", TemplateHandler),
+        ]
+        setttings = dict(
+            autoreload=True,
+            debug=True,
+            compress_response=True,
+            template_path=os.path.join(os.path.dirname(__file__), "templates"),
+            compiled_template_cache=False,
+            xsrf_cookies = True,
+        )
+        super(Application, self).__init__(handlers, **setttings)
+
 
 def make_app():
-	return Application()
+    return Application()
 
 if __name__ == "__main__":
-	app = make_app()
-	app.listen(8888)
-	tornado.ioloop.IOLoop.current().start()
+    app = make_app()
+    app.listen(8888)
+    tornado.ioloop.IOLoop.current().start()
---
+++ run_autopep8.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+
+autopep8 --ignore=W602,E501,E301,E309 -i *.py
